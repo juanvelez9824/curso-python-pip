5617e1dfdfd8761da8122fad7f28dd616007e07a
Juan David <juan.velez.duque@correounivalle.edu.co>
2023-07-07 14:35:20 -0500

    deploy

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,79 +1,47 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
-from typing import Optional
-
+from jwt_manager import create_token
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+from routers.user import user_router
 app = FastAPI()
-app.title="Mi aplicacion con FastAPI"
+app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+app.add_middleware(ErrorHandler)
+app.include_router(movie_router)
+app.include_router(user_router)
 
+Base.metadata.create_all(bind=engine)
 
-movies = [{
-    "id":1,
-    "title":"Avatar",
-    "overview":"en un exuberante planeta llamado pandora viven lo na´vi",
-    "year": "2009",
-    "rating":7.8,
-    "category":"Accion"
-}
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
 ]
-@app.get("/",tags=["Home"])
 
+
+
+
+@app.get('/', tags=['home'])
 def message():
-    return HTMLResponse("<h1>hello world </h1>")
-
-@app.get("/movies", tags=["movies"])
-def get_movies():
-    return movies
+    return HTMLResponse('<h1>Hello world</h1>')
 
 
-@app.get("/movies/{id}" ,tags=["movies"])
-def get_movies(id:int):
-    for item in movies:
-        if item["id"]==id:
-            return item
-    return []
-@app.get("/movies/",tags=["movies"])
-def get_movies_by_category(category:str,year:int):
-    return [ item for item in movies if item["category"] == category]
-
-
-@app.post("/movies",tags=["movies"])
-def create_movie(movie : Movie):
-    movies.append(movie)
-    return movies
-
-
-@app.put("/movies/{id}", tags=["movies"])
-def update_movie(id:int, movie:Movie):
-    for item in movies:
-         if item["id"]==id:
-             item["title"]= movie.title
-             item["overview"]=movie.overview
-             item["year"] = movie.year
-             item["rating"] = movie.rating
-             item["category"] = movie.category
-             return movies
-
-
-@app.delete("/movies/{id}",tags=["movies"])
-def delete_movies(id: int):
-    for item in movies:
-         if item["id"]==id:
-             movies.remove(item)
-             return movies
-             
-
-
-
-             
 
        
